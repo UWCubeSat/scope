10d0679ce99f435a7cd989aefcd2b5899b346879
refact0r <34758569+refact0r@users.noreply.github.com>
2026-05-20 07:18:15 +0000

    add more noise filter tests

--- test/scope/noise-filter/noise-filter-test.cpp
+++ test/scope/noise-filter/noise-filter-test.cpp
@@ -70,4 +70,94 @@ TEST(DarkScreenFilterTest, ThrowsOnMismatchedDimensions) {
     EXPECT_THROW(filter.Run({a, b}), std::runtime_error);
 }
 
+TEST(DarkScreenFilterTest, ThrowsOnMismatchedChannels) {
+    DarkScreenFilter filter;
+
+    std::array<unsigned char, 2> imageA{1, 2};
+    std::array<unsigned char, 4> imageB{1, 2, 3, 4};
+
+    Image a{1, 2, 1, imageA.data()};
+    Image b{1, 2, 2, imageB.data()};
+
+    EXPECT_THROW(filter.Run({a, b}), std::runtime_error);
+}
+
+TEST(DarkScreenFilterTest, ThrowsOnNullReferenceImage) {
+    DarkScreenFilter filter;
+
+    Image a{2, 2, 1, nullptr};
+
+    EXPECT_THROW(filter.Run({a}), std::runtime_error);
+}
+
+TEST(DarkScreenFilterTest, ThrowsOnNullSubsequentImage) {
+    DarkScreenFilter filter;
+
+    std::array<unsigned char, 4> imageA{1, 2, 3, 4};
+    Image a{2, 2, 1, imageA.data()};
+    Image b{2, 2, 1, nullptr};
+
+    EXPECT_THROW(filter.Run({a, b}), std::runtime_error);
+}
+
+TEST(DarkScreenFilterTest, ThrowsOnNonPositiveDimensions) {
+    DarkScreenFilter filter;
+
+    std::array<unsigned char, 1> data{1};
+    Image zeroWidth{0, 2, 1, data.data()};
+    Image zeroHeight{2, 0, 1, data.data()};
+    Image zeroChannels{2, 2, 0, data.data()};
+
+    EXPECT_THROW(filter.Run({zeroWidth}), std::runtime_error);
+    EXPECT_THROW(filter.Run({zeroHeight}), std::runtime_error);
+    EXPECT_THROW(filter.Run({zeroChannels}), std::runtime_error);
+}
+
+TEST(DarkScreenFilterTest, SingleImageReturnsCopy) {
+    DarkScreenFilter filter;
+
+    std::array<unsigned char, 4> data{7, 42, 0, 255};
+    Image a{2, 2, 1, data.data()};
+
+    Image dark = filter.Run({a});
+
+    ASSERT_EQ(dark.width, 2);
+    ASSERT_EQ(dark.height, 2);
+    ASSERT_EQ(dark.channels, 1);
+    ASSERT_NE(dark.image, data.data());
+    EXPECT_EQ(dark.image[0], 7);
+    EXPECT_EQ(dark.image[1], 42);
+    EXPECT_EQ(dark.image[2], 0);
+    EXPECT_EQ(dark.image[3], 255);
+
+    std::free(dark.image);
+}
+
+TEST(DarkScreenFilterTest, MultiChannelMediansChannelsIndependently) {
+    DarkScreenFilter filter;
+
+    // 1x2 image, 3 channels, layout per pixel: [R, G, B].
+    std::array<unsigned char, 6> imageA{10, 100, 200, 50, 60, 70};
+    std::array<unsigned char, 6> imageB{20, 110, 180, 40, 80, 90};
+    std::array<unsigned char, 6> imageC{30, 90, 220, 60, 70, 80};
+
+    Image a{1, 2, 3, imageA.data()};
+    Image b{1, 2, 3, imageB.data()};
+    Image c{1, 2, 3, imageC.data()};
+
+    Image dark = filter.Run({a, b, c});
+
+    ASSERT_EQ(dark.channels, 3);
+    // Pixel 0: R={10,20,30}->20, G={100,110,90}->100, B={200,180,220}->200.
+    EXPECT_EQ(dark.image[0], 20);
+    EXPECT_EQ(dark.image[1], 100);
+    EXPECT_EQ(dark.image[2], 200);
+    // Pixel 1: R={50,40,60}->50, G={60,80,70}->70, B={70,90,80}->80.
+    EXPECT_EQ(dark.image[3], 50);
+    EXPECT_EQ(dark.image[4], 70);
+    EXPECT_EQ(dark.image[5], 80);
+
+    std::free(dark.image);
+}
+
 }  // namespace scope
